4032ed57c594cda31c8d2cf4c5994cc5e8d32875
Nrn Shukla <narendra1192cs@gmail.com>
2022-01-04 12:17:19 +0000

    adding long and short-lambda usage for interest calculator

--- src/main/java/eight/functionInterface/InterestTester.java
+++ src/main/java/eight/functionInterface/InterestTester.java
@@ -7,17 +7,20 @@ public class InterestTester {
 	public static void main(String args[]) {
 		
 		List<Float> amounts = Arrays.asList(20000f,35000f,40000f,45000f);
-		float interestRate = 3.99f; 
+		float interestRate = 3.99f;
+
+		InterestCalculator interestCalculator = (amt, intrestRate)->{
+			return amt*intrestRate;
+		};
+		InterestCalculator ic= new InterestCalculator() {
+			@Override
+			public float calculateInterestAmount(float amount, float interestRate) {
+				return 0;
+			}
+		};
 		
 		amounts.stream()
-				.forEach(amount -> System.out.println(
-						new InterestCalculator() {
-							@Override
-							public float calculateInterestAmount(float amount,
-									float interestRate) {
-								return amount * interestRate;
-							}
-						}.calculateInterestAmount(amount, 0.99f)	
+				.forEach(amount -> System.out.println(interestCalculator.calculateInterestAmount(amount, 0.99f)
 						)
 				);
 		
